32de6240d7ea73e69d86f684306ba307303b7af4
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-22 16:36:10 +0100

    prueba gitignore

--- cs/obj/Debug/net8.0/Cine-API.AssemblyInfo.cs
+++ cs/obj/Debug/net8.0/Cine-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cine-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eebae2925ac1660223a60fbfd7716f152c7bd808")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a225d94c27990450df9b44d03993dfa9b4c694")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cine-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cine-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
